7e1414212287533b7b19317d67122be5506878d4
Mamunur Rashid <30565480+mamunur105@users.noreply.github.com>
2021-12-05 00:53:13 +0600

    option and meta field

--- Apps/Traits/Getdata.php
+++ Apps/Traits/Getdata.php
@@ -23,15 +23,17 @@ trait Getdata {
 				global $post;
 				$post_id = $post->ID;
 			}
-			$values = get_post_meta( $post->ID );
+			$values = get_post_meta( $post_id );
 			$values = apply_filters( 'pico_prepare_meta_data', $values, $this->settings );
+			return $this->meta_data_prepare( $values );
 		}
 		if ( 'option' === $this->settings['settings_type'] ) {
 			$values = get_option( $field_id );
 			$values = apply_filters( 'pico_prepare_option_data', $values, $this->settings );
+			return $values;
 		}
 
-		return $this->data_prepare( $values );
+		return $values;
 	}
 
 	/**
@@ -40,17 +42,22 @@ trait Getdata {
 	 * @param [array] $values settings value.
 	 * @return array
 	 */
-	public function data_prepare( $values ) {
+	public function meta_data_prepare( $values ) {
 		$new_value = array();
 		if ( empty( $values ) ) {
 			return $new_value;
 		}
 		foreach ( $this->settings['fields'] as $fld ) {
 			if ( isset( $fld['id'] ) && isset( $values[ $fld['id'] ] ) ) {
-
-				$new_value[ $fld['id'] ] = $values[ $fld['id'] ];
+				if ( isset( $values[ $fld['id'] ][0] ) ) {
+					$value = maybe_unserialize( $values[ $fld['id'] ][0] );
+				}else{
+					$value = maybe_unserialize( $values[ $fld['id'] ] );
+				}
+				$new_value[ $fld['id'] ] = $value;
 			}
 		}
+		error_log( print_r( $new_value, true ), 3, __DIR__ . '/log.txt' );
 		return $new_value;
 	}
 
